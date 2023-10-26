cc93dac68da30c3cf1c9f91f7a647e31d848f858
Kritagya <er.kritagyachopra@gmail.com>
2023-10-26 17:45:02 +0530

    Assignment01 completed

---
+++ Assignment01/src/solution02/Employee.java
@@ -0,0 +1,33 @@
+package solution02;
+
+public class Employee {
+	private int empid;
+	private String name;
+	private double sal;
+	public Employee() {
+	}
+	public Employee(int empid, String name, double sal) {
+		this.empid = empid;
+		this.name = name;
+		this.sal = sal;
+	}
+	public int getEmpid() {
+		return empid;
+	}
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getSal() {
+		return sal;
+	}
+	public void setSal(double sal) {
+		this.sal = sal;
+	}
+	
+}
